d7ecece4ffc315347b70f595186a7dfd97e25e86
Matthew Rockett <rockettm@cs.washington.edu>
2019-08-08 14:41:19 -0700

    Run through the linting gauntlet

---
+++ src/testing/test_pf.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import time
+
+import message_filters
+import numpy as np
+
+# Test File for PF
+# Matt Schmittle
+# DO NOT EDIT
+import rospy
+from geometry_msgs.msg import PoseStamped
+from matplotlib import pyplot as plt
+
+import utils as Utils
+
+error_array = []
+pf_array = []
+true_array = []
+started = False
+plotted = False
+
+
+def callback(true_pose, pf_pose):
+    global error_array, start_time, started, plotted
+    if not started:
+        start_time = time.time()
+        started = True
+    error_x = (true_pose.pose.position.x - pf_pose.pose.position.x) ** 2
+    error_y = (true_pose.pose.position.y - pf_pose.pose.position.y) ** 2
+    error_t = (
+        Utils.quaternion_to_angle(true_pose.pose.orientation)
+        - Utils.quaternion_to_angle(pf_pose.pose.orientation)
+    ) ** 2
+    error_array.append([error_x, error_y, error_t])
+    print_array = np.array(error_array)
+    print(
+        "Median X Error:",
+        np.median(print_array[:, 0]),
+        "Median Y Error:",
+        np.median(print_array[:, 1]),
+        "Median Theta Error:",
+        np.median(print_array[:, 2]),
+    )
+    true_array.append([true_pose.pose.position.x, true_pose.pose.position.y])
+    pf_array.append([pf_pose.pose.position.x, pf_pose.pose.position.y])
+
+    # Plot trajectories
+    if plot and time.time() - start_time > 40 and not plotted:
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.plot(np.array(true_array)[:, 0], np.array(true_array)[:, 1], c="r")
+        plt.plot(np.array(pf_array)[:, 0], np.array(pf_array)[:, 1], c="g")
+        plt.show()
+        plotted = True
+
+
+if __name__ == "__main__":
+    rospy.init_node("testpf", anonymous=True)
+    gt_topic = str(rospy.get_param("~gt_topic", "/pf/ta/viz/inferred_pose"))
+    plot = bool(rospy.get_param("~plot", False))
+
+    # Time Synchronizer
+    pose_sub = message_filters.Subscriber("/pf/viz/inferred_pose", PoseStamped)
+    pf_sub = message_filters.Subscriber(gt_topic, PoseStamped)
+
+    ts = message_filters.ApproximateTimeSynchronizer([pose_sub, pf_sub], 10, 0.1)
+    ts.registerCallback(callback)
+    rospy.spin()
---
+++ src/testing/test_rs.py
@@ -0,0 +1,43 @@
+# Testing Code for ReSample.py
+# Matt Schmittle
+# DO NOT EDIT
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from resample import ReSampler
+
+if __name__ == "__main__":
+    n_particles = 100  # The number of particles
+    k_val = 50  # Number of particles that have non-zero weight
+    trials = 10  # The number of re-samplings to do
+    num_figs = 3  # number of figures to make
+
+for f in range(0, num_figs):
+    k_val = (f + 1) * 30
+    histogram = np.zeros(n_particles, dtype=np.float)  # Keeps track of how many times
+    # each particle has been sampled
+    # across trials
+    for i in range(trials):
+        particles = np.repeat(
+            np.arange(n_particles)[:, np.newaxis], 3, axis=1
+        )  # Create a set of particles
+        # Here their value encodes their index
+        # Have increasing weights up until index k_val
+        weights = np.arange(n_particles, dtype=np.float)
+        weights[k_val:] = 0.0
+        weights[:] = weights[:] / np.sum(weights)
+
+        rs = ReSampler(particles, weights)  # Create the Resampler
+        rs.resample_low_variance()
+
+        # Add the number times each particle was sampled
+        for j in range(particles.shape[0]):
+            histogram[particles[j, 0]] = histogram[particles[j, 0]] + 1
+
+    # Display as histogram
+    plt.figure("k = " + str(k_val))
+    plt.bar(np.arange(n_particles), histogram)
+    plt.xlabel("Particle Idx")
+    plt.ylabel("# Of Times Sampled")
+plt.show()
