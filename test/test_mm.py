4ef649650cd315035c0290f4ef408bcb3841fed6
Your Name <schmttle@udel.edu>
2020-02-12 13:33:59 -0800

    formating

--- test/test_mm.py
+++ test/test_mm.py
@@ -3,16 +3,18 @@
 # DO NOT EDIT
 
 import math
+import os
+import sys
 import unittest
 from threading import Lock
 
 import matplotlib.pyplot as plt
 import numpy as np
-import sys
-import os
+
+from motion_model import KinematicMotionModel
+
 pf_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../src'),)
 sys.path.append(pf_path)
-from motion_model import KinematicMotionModel
 
 # Radius to count particles around ground truth
 RADIUS = 0.25
