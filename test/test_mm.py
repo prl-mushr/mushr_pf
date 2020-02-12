7e7211003bdf0fb7bcd99da41f2885f9d872a395
Matthew Rockett <rockettm@cs.washington.edu>
2020-02-12 14:50:57 -0800

    Fix linting and test

--- test/test_mm.py
+++ test/test_mm.py
@@ -3,18 +3,13 @@
 # DO NOT EDIT
 
 import math
-import os
-import sys
 import unittest
 from threading import Lock
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-from motion_model import KinematicMotionModel
-
-pf_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../src'),)
-sys.path.append(pf_path)
+from mushr_pf.motion_model import KinematicMotionModel
 
 # Radius to count particles around ground truth
 RADIUS = 0.25
