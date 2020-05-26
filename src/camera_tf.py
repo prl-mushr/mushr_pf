5c1d8e4f79a7d14c23affb3c29862ffd955e2ee5
Stefan <stef.layanto@gmail.com>
2020-05-26 11:29:14 -0700

    fixed camera odom transform

--- src/camera_tf.py
+++ src/camera_tf.py
@@ -10,6 +10,7 @@ import tf
 from utils import quaternion_to_angle
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Pose, Point, PoseWithCovarianceStamped
+from scipy.spatial.transform import Rotation as R
 
 class CameraTf:
     """                                                                                                                                                                                                    
@@ -24,6 +25,7 @@ class CameraTf:
             cam_frame: The name of frame of the camera
             map_fram: The name of the frame of the map
         """
+        print "Initializing Camera Tf"
         self.cam_odom_topic = cam_odom_topic
         self.cam_frame = cam_frame
         self.map_frame = map_frame
@@ -48,7 +50,7 @@ class CameraTf:
 
         # Subscriber to update camera odometry and publish transform
         self.cam_sb = rospy.Subscriber(
-            cam_odom_topic,Odometry, self.send_camera_tf
+            cam_odom_topic, Odometry, self.cam_odom_cb
         )
 
         # Transform broadcaster
@@ -60,45 +62,75 @@ class CameraTf:
                                   rospy.Time.now(),
                                   self.cam_frame,
                                   self.map_frame)
+        print "camera_tf: Initialized"
 
     def clicked_pose_cb(self, msg):
         """
           Sets the target pose to be the given 2D pose estimate, then updates the transformation
-          based on the new target pose and the current camera pose. 
+          based on the new target pose and the current camera pose. Also sends the tf
             msg: geometry_msgs/PoseWithCovarianceStamped
         """
+        # Do nothing if no initial pose given
+        if self.curr_pose is None:
+            print "camera_tf: Haven't Recieved Camera Data Yet"
+            return
+
         # Update target pose
         self.target_pose = msg.pose.pose
 
-        # Compute target and current point/orientation
+        # Compute target yaw and offset
+        target_yaw = quaternion_to_angle(self.target_pose.orientation)
+        curr_yaw = quaternion_to_angle(self.curr_pose.orientation)
+        self.rotation = tf.transformations.quaternion_from_euler(0, 0, target_yaw - curr_yaw)
+        
+        # Get target point in map frame, and current point in camera frame
         target_point = (
             self.target_pose.position.x,
             self.target_pose.position.y,
             self.target_pose.position.z
         )
+
         curr_point = (
             self.curr_pose.position.x,
             self.curr_pose.position.y,
             self.curr_pose.position.z
         )
-        target_yaw = quaternion_to_angle(self.target_pose.orientation)
-        curr_yaw = quaternion_to_angle(self.curr_pose.orientation)
 
-        # translation is target point - current point
-        self.translation = tuple(map(lambda i, j: i - j, target_point, curr_point))
-        
-        # rotation is target yaw - current yaw
-        self.rotation = tf.transformations.quaternion_from_euler(0, 0, target_yaw - curr_yaw)
+        # Apply rotation matrix to transform curr point to map frame
+        matrix = R.from_quat(self.rotation)
+        tf_curr_point = matrix.apply(curr_point)
 
-    def send_camera_tf(self, msg):
+        # Compute translation between both points now in map frame
+        self.translation = tuple(map(lambda i, j: i - j, target_point, tf_curr_point))
+
+#        print "Curr point: ", curr_point
+#        print "Tf Curr point: ", tf_curr_point
+#        print "Target point:", target_point
+#        print "tf: ", self.translation, " yaw from ", curr_yaw, " to ", target_yaw
+
+#        self.rotation = tf.transformations.quaternion_from_euler(0, 0, 0) # No Rotation
+
+        # Send Transform
+        self.send_tf()
+
+
+    def cam_odom_cb(self, msg):
         """
-          Updates the camera pose and then publishes the transformation
+          Updates the cached camera pose
             msg: nav_msgs/Odometry
         """
+        if self.curr_pose is None:
+            self.curr_pose = msg.pose.pose
+            print "camera_tf: Recieved Initial Pose"
+            return
+
         # Update current camera pose
         self.curr_pose = msg.pose.pose
 
-        # TODO: Might change to send at lower Hz since pose topic at 200Hz
+    def send_tf(self):
+        """
+          Sends the current transform
+        """
         # Send Transform
         self.cam_br.sendTransform(
             self.translation,
@@ -114,8 +146,12 @@ if __name__ == '__main__':
     cam_odom_topic = "/camera/odom/sample"
     cam_frame = "/camera_odom_frame"
     map_frame = "/map"
-    try:
-        camera_tf = CameraTf(cam_odom_topic, cam_frame, map_frame)
-        rospy.spin()
-    except KeyboardInterrupt:
-        rospy.loginfo('Shutting down')
+    rate = rospy.Rate(20) # Rate set to 20Hz
+    camera_tf = CameraTf(cam_odom_topic, cam_frame, map_frame)
+    while not rospy.is_shutdown():
+        try:
+            camera_tf.send_tf()
+            rate.sleep()
+#            rospy.spin()
+        except KeyboardInterrupt:
+            rospy.loginfo('Shutting down')
